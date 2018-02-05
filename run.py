7dd91f1b62ebcd4965e4862e2c66087d8b54a48d
Dean Flint <dean.p.flint@gmail.com>
2018-02-05 12:20:21 +0000

    Message dictionary created to replace the list - which will allow flexibility when returning values.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect
 
 app = Flask(__name__)
@@ -6,11 +7,13 @@ messages = []
 
 def add_messages(username, message):
     """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
     
 def get_all_messages():
     """Get all of the messages and seperate them by a `br` """
-    return "<br>".join(messages)
+    return messages
 
 @app.route('/')
 def index():
